add128a5a36d2db296f2f57f088ffc0c4060fb42
dimasgardenia <dimas.gardenia@gmail.com>
2017-12-08 01:27:50 +0700

    palindrome done

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+function Palindrome(str) {
+
+  str = str.replace(/ /g,"").toLowerCase();
+  var compareStr = str.split("").reverse().join("");
+
+  if (compareStr === str) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
+}
+
+console.log(Palindrome('Deleveled'))
